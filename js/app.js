e9033470896c0743de83e4b05a5ec2d0459270f1
BryanG <BryanG@Bryans-MacBook-Pro.local>
2019-10-01 15:30:10 -0500

    Instatiated Tomagotchi

--- js/app.js
+++ js/app.js
@@ -1,1 +1,45 @@
 console.log("test");
+
+
+class Tomagotchi {
+	constructor(){
+		this.age = (Math.floor(Math.random() * 10) + 1);
+		this.hunger = (Math.floor(Math.random() * 10) + 1);
+		this.sleepiness = (Math.floor(Math.random() * 10) + 1);
+		this.boredom = (Math.floor(Math.random() * 10) + 1);
+	}
+//	die()
+
+//	sleep()
+
+
+};
+
+
+// listeners
+
+function tomagotchiTime() {
+	game.spawnToma();
+}
+
+
+
+
+
+
+
+
+// game logic
+const game = {
+
+	time: null,
+
+	spawnToma:function() {
+		const tomagotchiBorn = new Tomagotchi();
+		console.log(tomagotchiBorn);
+	}
+}
+
+
+
+tomagotchiTime();
